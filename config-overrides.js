8dc931748fca5c26eba30caa7191b1f20b22e727
daesoo94 <tlaeotn123@gmail.com>
2021-10-03 07:29:34 +0900

    dev multi threading 추가.

--- config-overrides.js
+++ config-overrides.js
@@ -4,10 +4,10 @@ module.exports = {
       const config = configFunction(proxy, allowedHost);
 
       // Set header for using shared array buffer for shared memory with worker thread
-      // config.headers = {
-      //   "Cross-Origin-Opener-Policy": "same-origin",
-      //   "Cross-Origin-Embedder-Policy": "require-corp"
-      // };
+      config.headers = {
+        "Cross-Origin-Opener-Policy": "same-origin",
+        "Cross-Origin-Embedder-Policy": "require-corp"
+      };
       config.liveReload = false;
 
       return config;
